bdc729e4b37d9edfc14419dad69f04b0201c3a77
davidgb <davidgb@292c5d04-7d17-4f45-9e79-28f3f884e7fc>
2010-09-09 04:52:44 +0000

    Updates from John. Digilent S3STARTER and XSA-3S1000 work.

---
+++ rtl/VHDL/dma6844.vhd
@@ -0,0 +1,243 @@
+--===========================================================================--
+--                                                                           --
+--               Synthesizable 6844 Compatible DMA Controller                --
+--                                                                           --
+--===========================================================================--
+--
+--  File name      : dma6844.vhd
+--
+--  Entity name    : dma6844
+--
+--  Purpose        : Implements a 6844 compatible Direct Memory Access Controller
+--                   It is intended for use with 68xx compatible FPGA SoCs.
+--                  
+--  Dependencies   : ieee.std_logic_1164
+--                   ieee.std_logic_unsigned
+--                   ieee.std_logic_arith
+--                   unisim.vcomponents
+--
+--  Author         : John E. Kent
+--
+--  Email          : dilbert57@opencores.org      
+--
+--  Web            : http://opencores.org/project,system09
+--
+--  Registers      :
+--
+--  4 Channel version
+--
+--  IO +  0 = DMA_AH0 = Address Register 0 High
+--  IO +  1 = DMA_AL0 = Address Register 0 Low
+--  IO +  2 = DMA_CH0 = Count   Register 0 High
+--  IO +  3 = DMA_CL0 = Count   Register 0 Low
+--
+--  IO +  4 = DMA_AH1 = Address Register 1 High
+--  IO +  5 = DMA_AL1 = Address Register 1 Low
+--  IO +  6 = DMA_CH1 = Count   Register 1 High
+--  IO +  7 = DMA_CL1 = Count   Register 1 Low
+-- 
+--  IO +  8 = DMA_AH2 = Address Register 2 High
+--  IO +  9 = DMA_AL2 = Address Register 2 Low
+--  IO + 10 = DMA_CH2 = Count   Register 2 High
+--  IO + 11 = DMA_CL2 = Count   Register 2 Low
+-- 
+--  IO + 12 = DMA_AH3 = Address Register 3 High
+--  IO + 13 = DMA_AL3 = Address Register 3 Low
+--  IO + 14 = DMA_CH3 = Count   Register 3 High
+--  IO + 15 = DMA_CL3 = Count   Register 3 Low
+--  
+--  IO + 16 = DMA_CC0 = Channel Control Register 0
+--  IO + 17 = DMA_CC1 = Channel Control Register 1
+--  IO + 18 = DMA_CC2 = Channel Control Register 2
+--  IO + 19 = DMA_CC3 = Channel Control Register 3
+--    Bit[7] = DMA_DEF = DMA END FLAG (DEND)
+--    Bit[6] = DMA_BSY = DMA BUSY FLAG (READ ONLY)
+--    Bit[3] = DMA_AUD = DMA ADDRESS NOT UP/DOWN
+--    Bit[2] = DMA_MCA = DMA MODE CONTROL 0=>DRQ2   1=>DRQ1 
+--    Bit[1] = DMA_MCB = DMA MODE CONTROL 0=>SINGLE 1=>BLOCK 
+--       0 0 = DMA_MD2 = DMA MODE 2 - SINGLE TRANSFER - DRQ2
+--       0 1 = DMA_MD3 = DMA MODE 3 - BLOCK  TRANSFER - DRQ2
+--       1 0 = DMA_MD1 = DMA MODE 1 - SINGLE TRANSFER - DRQ1
+--       1 1 = DMA_MDU = DMA MODE 4 - BLOCK  TRANSFER - DRQ1 - ACTUALLY UDEFINED 
+--    Bit[0] = DMA_RW  = DMA READ/NOT WRITE
+--
+--  IO + 20  = DMA_PRI = DMA Priority Control Register
+--    Bit[7] = DMA_ROT = DMA Rotate Control 0=>FIXED 1=>ROTATE
+--    Bit[3] = DMA_RE3 = DMA REQUEST ENABLE #3
+--    Bit[2] = DMA_RE2 = DMA REQUEST ENABLE #2
+--    Bit[1] = DMA_RE1 = DMA REQUEST ENABLE #1
+--    Bit[0] = DMA_RE0 = DMA REQUEST ENABLE #0
+-- 
+--  IO + 21  = DMA_INT = DMA Interrupt Control Register
+--    Bit[7] = DMA_IEF = DMA END IRQ FLAG
+--    Bit[3] = DMA_IE3 = DMA END IRQ ENABLE #3
+--    Bit[2] = DMA_IE2 = DMA END IRQ ENABLE #2
+--    Bit[1] = DMA_IE1 = DMA END IRQ ENABLE #1
+--    Bit[0] = DMA_IE0 = DMA END IRQ ENABLE #0
+--  
+--  IO + 22  = DMA_CHN = DMA Data Chain register
+--    Bit[3] = DMA_C24 = TWO/FOUR CHANNEL SELECT
+--    Bit[2] = DMA_DCB = DATA CHAIN CHANNEL SELECT B
+--    Bit[1] = DMA_DCA = DATA CHAIN CHANNEL SELECT A
+--    Bit[0] = DMA_DCE = DATA CHAIN ENABLE 
+--
+--  Copyright (C) 2010 John Kent
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+--===========================================================================--
+--                                                                           --
+--                              Revision  History                            --
+--                                                                           --
+--===========================================================================--
+--
+-- Revision Author        Date               Description
+-- 0.1      John E. Kent  18th April 2010    Initial release
+--
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_unsigned.all;
+  use ieee.std_logic_arith.all;
+
+library unisim;
+  use unisim.vcomponents.all;
+
+entity dma6844 is
+   generic (
+      ADDR_WIDTH = 16;
+      DATA_WIDTH = 8;
+      CHAN_COUNT = 4
+      )
+	port (
+      --
+      -- CPU Slave Interface
+      --	
+		clk      : in  std_logic;
+		rst      : in  std_logic;
+		rw       : out std_logic;
+		cs       : out std_logic;
+		addr     : in  std_logic_vector(LOG2(CHAN_COUNT*4*ADDR_WIDTH/DATA_WIDTH)-1 downto 0);
+	   data_in  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+	   data_out : out std_logic_vector(DATA_WIDTH-1 downto 0);
+      irq      : out std_logic;
+      --
+      -- Bus Master Interface
+      --
+      breq     : out std_logic;
+      bgnt     : in  std_logic;
+      brw      : out std_logic;
+      bvma     : out std_logic;
+      baddr    : out std_logic_vector(ADDR_WIDTH-1 downto 0);
+      --
+      -- Device Interface
+      --
+      txreq    : in  std_logic_vector(CHAN_COUNT-1 downto 0);
+      txstb    : out std_logic_vector(CHAN_COUNT-1 downto 0);
+      txack    : out std_logic_vector(CHAN_COUNT-1 downto 0);
+      txend    : out std_logic_vector(CHAN_COUNT-1 downto 0)
+		);
+end dma6844;
+
+architecture rtl of dma6844 is
+
+constant REG_COUNT : integer = (CHAN_COUNT * 2 * ADDR_WIDTH / DATA_WIDTH) + DMA_CHAN + 3;
+
+subtype addr_subtype is std_logic_vector(ADDR_WIDTH-1 downto 0);
+subtype data_subtype is std_logic_vector(DATA_WIDTH-1 downto 0);
+
+type addr_type is array(0 to CHAN_COUNT-1) of addr_subtype;
+type data_type is array(0 to CHAN_COUNT-1) of data_subtype;
+type reg_type  is array(0 to REG_COUNT-1)  of data_subtype;
+
+signal dma_addr    : addr_type;
+signal dma_count   : addr_type;
+signal dma_in_reg  : reg_type;
+signal dma_out_reg : reg_type;
+
+signal dma_reg_wr : std_logic := '0';
+signal dma_reg_rd : std_logic := '0';
+
+--
+-- Registers
+--
+signal dma_adh_reg : data_type;
+signal dma_adl_reg : data_type;
+signal dma_cth_reg : data_type;
+signal dma_ctl_reg : data_type;
+signal dma_chc_reg : data_type;
+signal dma_pri_reg : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal dma_irq_reg : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal dma_chn_reg : std_logic_vector(DATA_WIDTH-1 downto 0);
+
+begin
+--
+-- Write to DMA input register
+--
+dma_reg_write : process( clk, rst )
+variable reg_addr   : integer := 0;
+begin
+  if( falling_edge(clk) )
+    if( rst = '1' ) then
+      for i in 0 to CHAN_COUNT-1 loop
+        dma_addr(i)   <= (others=>'0');
+        dma_count(i)  <= (others=>'0');
+      end loop;
+      for i in 0 to REG_COUNT-1 loop
+        dma_in_reg(i) <= (others=>'0');
+      end loop;
+    else
+      if( cs='1' and rw='0' ) then
+        reg_addr := conv_integer(addr(ADDR_WIDTH-1 downto 0));
+        if( reg_addr < REG_COUNT ) then
+          dma_in_reg(reg_addr) <= data_in;
+        end if;
+      end if;
+    end if;
+  end if;
+end process;
+
+--
+-- Assign input register to specific register names
+--
+dma_reg_assign : process( dma_in_reg )
+begin
+  for i in 0 to CHAN_COUNT-1 loop
+    dma_adh_reg(i) <= dma_in_reg((i*4)+0);
+    dma_adl_reg(i) <= dma_in_reg((i*4)+1);
+    dma_cth_reg(i) <= dma_in_reg((i*4)+2);
+    dma_ctl_reg(i) <= dma_in_reg((i*4)+3);
+    dma_chc_reg(i) <= dma_in_reg((CHAN_COUNT*4)+i);
+  end loop;
+  dma_pri_reg <= dma_in_reg((CHAN_COUNT*5)+0);
+  dma_irq_reg <= dma_in_reg((CHAN_COUNT*5)+1);
+  dma_chn_reg <= dma_in_reg((CHAN_COUNT*5)+2);
+end process;
+
+--
+-- Process Transfer Request Inputs
+--
+dma_tx_req : process( clk, rst, txreq )
+begin
+  if( rising_edge( clk ) ) then
+    if( rst='1' ) then
+    else
+      for i in 0 to CHAN_COUNT-1 loop
+      end loop;
+    end if;
+  end if; 
+end process;
+    
+end architecture rtl;
+
---
+++ rtl/VHDL/twi-master.vhd
@@ -0,0 +1,399 @@
+--===========================================================================--
+--
+-- SYNTHEZIABLE VHDL TWO WIRE INTERFACE
+--
+--===========================================================================
+--
+-- This core adheres to the GNU public license  
+--
+-- Design units   : TWI Master core
+--
+-- File name      : twi.vhd
+--
+-- Purpose        : Implements an I2C master Interface 
+--                  
+-- Dependencies   : ieee.std_logic_1164
+--                  ieee.numeric_std
+--                  unisim.vcomponents
+--
+-- Revision list  :
+--
+-- Version Author        Date         Changes
+--
+-- 0.1     John Kent     2010-05-04   New model
+--
+--         dilbert57@opencores.org
+--
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library unisim;
+      use unisim.vcomponents.all;
+
+-----------------------------------------------------------------------
+-- Entity for TWI                                                    --
+-----------------------------------------------------------------------
+
+entity twi is
+  generic (
+    CLK_FREQ : integer := 25_000_000;
+  );
+
+  port (
+    --
+    -- CPU signals
+    --
+    clk      : in  std_logic;                     -- System Clock
+    rst      : in  std_logic;                     -- Reset input (active high)
+    cs       : in  std_logic;                     -- Chip Select
+    rw       : in  std_logic;                     -- Read / Not Write
+    irq      : out std_logic;                     -- Interrupt
+    addr     : in  std_logic;                     -- Register Select
+    data_in  : in  std_logic_vector(7 downto 0);  -- Data Bus In 
+    data_out : out std_logic_vector(7 downto 0);  -- Data Bus Out
+
+    -- I2C Signals
+    --
+    scl     : inout std_logic;                    -- serial clock
+    sda     : inout std_logic                     -- serial data
+    );
+end twi;  
+
+-------------------------------------------------------------------------------
+-- Architecture for Two Wire Interface registers
+-------------------------------------------------------------------------------
+
+architecture rtl of twi is
+
+  -----------------------------------------------------------------------------
+  -- Signals
+  -----------------------------------------------------------------------------
+
+  ----------------------------------------------------------------------
+  --  Status Register: twi_status_reg 
+  ----------------------------------------------------------------------
+  --
+  -- IO address + 0 Read
+  --
+  --+-------+-------+-------+-------+-------+-------+-------+-------+
+  --| RXIRQ | TXIRQ | ACKE  |       |       |       | TXRDY | RXRDY |
+  --+-------+-------+-------+-------+-------+-------+-------+-------+
+  -- RXIRQ - Bit[7] - Receive Interrupt Request
+  -- TXIRQ - Bit[6] - Transmit Interrupt Request
+  -- ACKE  - Bit[5] - Acknowledge Error
+  -- TXRDY - Bit[1] - Transmit Ready (byte transmitted)
+  -- RXRDY - Bit[0] - Receive Ready (byte received)
+  -- 
+  signal twi_status_reg : std_logic_vector(7 downto 0) := (others => '0');
+
+  ----------------------------------------------------------------------
+  --  Control Register: twi_control_reg
+  ----------------------------------------------------------------------
+  --
+  -- IO address + 0 Write
+  --
+  --+--------+-------+--------+--------+--------+--------+--------+--------+
+  --|  RXIE  | TXIE  | TWPS(1)| TWPS(0)| TWBR(3)| TWBR(2)| TWBR(1)| TWBR(0)|
+  --+--------+-------+--------+--------+--------+--------+--------+--------+
+  -- RXIE - Bit[7]
+  -- 0       - Rx Interrupt disabled
+  -- 1       - Rx Interrupt enabled
+  -- TXIE - Bit[6]
+  -- 0       - Tx Interrupt disabled
+  -- 1       - Tx Interrupt enabled
+  --
+  -- SCL frequency = CPU Clock Frequency / ( 16 + 2(TWBR) . 4^TWPS)
+  --
+  -- TWPS - Bits[5..4]
+  -- 0 0     - Prescale by  1
+  -- 0 1     - Prescale by  4
+  -- 1 0     - Prescale by 16
+  -- 1 1     - Prescale by 64
+  --
+  -- TWBR - Bits[3..0]
+  -- 0 0 0 0   - Baud Clk divide by  1
+  -- 0 0 0 1   - Baud Clk divide by  2
+  -- 0 0 1 0   - Baud Clk divide by  3
+  -- 0 0 1 1   - Baud Clk divide by  4
+  -- 0 1 0 0   - Baud Clk divide by  5
+  -- 0 1 0 1   - Baud Clk divide by  6
+  -- 0 1 1 0   - Baud Clk divide by  7
+  -- 0 1 1 1   - Baud Clk divide by  8
+  -- 1 0 0 0   - Baud Clk divide by  9
+  -- 1 0 0 1   - Baud Clk divide by 10
+  -- 1 0 1 0   - Baud Clk divide by 11
+  -- 1 0 1 1   - Baud Clk divide by 12
+  -- 1 1 0 0   - Baud Clk divide by 13
+  -- 1 1 0 1   - Baud Clk divide by 14
+  -- 1 1 1 0   - Baud Clk divide by 15
+  -- 1 1 1 1   - Baud Clk divide by 16
+  
+
+  signal twi_control_reg : std_logic_vector(7 downto 0) := (others => '0'); -- control register
+
+  ----------------------------------------------------------------------
+  -- Receive Register
+  ----------------------------------------------------------------------
+  --
+  -- IO address + 1     Read
+  --
+  signal twi_rx_reg : std_logic_vector(7 downto 0) := (others => '0');
+
+  ----------------------------------------------------------------------
+  -- Transmit Register
+  ----------------------------------------------------------------------
+  --
+  -- IO address + 1     Write
+  --
+  signal twi_tx_reg : std_logic_vector(7 downto 0) := (others => '0');
+  
+  signal Reset    : std_logic;          -- Reset (Software & Hardware)
+  signal RxRst    : std_logic;          -- Receive Reset (Software & Hardware)
+  signal TxRst    : std_logic;          -- Transmit Reset (Software & Hardware)
+  signal TxDbit   : std_logic;          -- Transmit data bit
+  signal RxDR     : std_logic := '0';   -- Receive Data ready
+  signal TxIdle   : std_logic;          -- Transmitter idle
+  signal TxBE     : std_logic := '0';   -- Transmit buffer empty
+  signal TxAck    : std_logic;          -- Byte transmitted to transmitter
+  --
+  signal FErr     : std_logic := '0';   -- Frame error
+  signal OErr     : std_logic := '0';   -- Output error
+  signal PErr     : std_logic := '0';   -- Parity Error
+  --
+  signal TxIEnb   : std_logic := '0';   -- Transmit interrupt enable
+  signal RxIEnb   : std_logic := '0';   -- Receive interrupt enable
+  --
+  signal ReadRR   : std_logic := '0';   -- Read receive buffer
+  signal WriteTR  : std_logic := '0';   -- Write transmit buffer
+  signal ReadSR   : std_logic := '0';   -- Read Status register
+  --
+  signal DCDState : DCD_State_Type;     -- DCD Reset state sequencer
+  signal DCDDel   : std_logic := '0';   -- Delayed DCD_n
+  signal DCDEdge  : std_logic := '0';   -- Rising DCD_N Edge Pulse
+  signal DCDInt   : std_logic := '0';   -- DCD Interrupt
+
+begin
+  -----------------------------------------------------------------------------
+  -- Instantiation of internal components
+  -----------------------------------------------------------------------------
+
+  RxDev : entity ACIA_RX port map (
+    Clk    => clk,
+    RxRst  => RxRst,
+    RxRd   => ReadRR,
+    WdFmt  => CtrlReg(4 downto 2),
+    BdFmt  => CtrlReg(1 downto 0),
+    RxClk  => RxC,
+    RxDat  => RxD,
+    RxFErr => FErr,
+    RxOErr => OErr,
+    RxPErr => PErr,
+    RxRdy  => RxDR,
+    RxDout => RecvReg
+   );
+
+  TxDev : entity ACIA_TX port map (
+    Clk   => clk,
+    Reset => TxRst,
+    Wr    => WriteTR,
+    Din   => TxReg,
+    WdFmt => CtrlReg(4 downto 2),
+    BdFmt => CtrlReg(1 downto 0),
+    TxClk => TxC,
+    Dat   => TxDbit,
+    Empty => TxIdle
+   );
+
+---------------------------------------------------------------
+-- ACIA Reset may be hardware or software
+---------------------------------------------------------------
+  ACIA_Reset : process(clk, rst)
+  begin
+    -- Asynchronous External reset
+    if rst = '1' then
+      Reset <= '1';
+    elsif falling_edge(clk) then
+      -- Synchronous Software reset
+      Reset <= CtrlReg(1) and CtrlReg(0);
+    end if;              
+
+  end process;
+
+  -- Transmitter reset
+  TxRst <= Reset;
+  -- Receiver reset
+  RxRst <= Reset or DCD_n;
+
+  -----------------------------------------------------------------------------
+  -- ACIA Status Register
+  -----------------------------------------------------------------------------
+
+  ACIA_Status : process(Reset, clk)
+  begin
+    if Reset = '1' then
+      StatReg <= (others => '0');
+    elsif falling_edge(clk) then
+      StatReg(0) <= RxDR;                  -- Receive Data Ready
+      StatReg(1) <= TxBE and (not CTS_n);  -- Transmit Buffer Empty
+      StatReg(2) <= DCDInt;                -- Data Carrier Detect
+      StatReg(3) <= CTS_n;                 -- Clear To Send
+      StatReg(4) <= FErr;                  -- Framing error
+      StatReg(5) <= OErr;                  -- Overrun error
+      StatReg(6) <= PErr;                  -- Parity error
+      StatReg(7) <= (RxIEnb and RxDR) or
+                    (RxIEnb and DCDInt) or
+                    (TxIEnb and TxBE);
+    end if;
+  end process;
+
+
+-----------------------------------------------------------------------------
+-- ACIA Transmit Control
+-----------------------------------------------------------------------------
+
+  ACIA_Control : process(CtrlReg, TxDbit)
+  begin
+    case CtrlReg(6 downto 5) is
+      when "00" =>                      -- Disable TX Interrupts, Assert RTS
+        TxD    <= TxDbit;
+        TxIEnb <= '0';
+      when "01" =>                      -- Enable TX interrupts, Assert RTS
+        TxD    <= TxDbit;
+        TxIEnb <= '1';
+      when "10" =>                      -- Disable Tx Interrupts, Clear RTS
+        TxD    <= TxDbit;
+        TxIEnb <= '0';
+      when "11" =>                      -- Disable Tx interrupts, Assert RTS, send break
+        TxD    <= '0';
+        TxIEnb <= '0';
+      when others =>
+        null;
+    end case;
+
+    RxIEnb <= CtrlReg(7);
+  end process;
+
+  tx_process : process(clk, reset)
+  begin
+    if reset = '1' then
+      WriteTR <= '0';
+      TxAck   <= '0';
+    elsif falling_edge(clk) then
+      WriteTR <= '0';
+      TxAck   <= '0';
+      if TxBE = '0' and TxIdle = '1' then
+        WriteTR <= '1';
+        TxAck   <= '1';
+      end if;
+    end if;
+  end process;
+        
+-----------------------------------------------------------------------------
+-- Generate Read / Write strobes.
+-----------------------------------------------------------------------------
+
+  ACIA_Read_Write : process(clk, Reset)
+  begin
+    if reset = '1' then
+      CtrlReg <= (others => '0');
+      TxReg   <= (others => '0');
+      ReadRR  <= '0';
+      ReadSR  <= '0';
+      TxBE    <= '1';
+    elsif falling_edge(clk) then
+      ReadRR <= '0';
+      ReadSR <= '0';
+      if TxAck = '1' then
+        TxBE <= '1';
+      end if;
+      if cs = '1' then
+        if Addr = '0' then              -- Control / Status register
+          if rw = '0' then              -- write control register
+            CtrlReg <= DataIn;
+          else                          -- read status register
+            ReadSR <= '1';
+          end if;
+        else                            -- Data Register
+          if rw = '0' then              -- write transmiter register
+            TxReg <= DataIn;
+            TxBE  <= '0';
+          else                          -- read receiver register
+            ReadRR <= '1';
+          end if;
+        end if;
+      end if;
+    end if;
+  end process;
+
+---------------------------------------------------------------
+-- Set Data Output Multiplexer
+--------------------------------------------------------------
+
+  ACIA_Data_Mux : process(Addr, RecvReg, StatReg)
+  begin
+    if Addr = '1' then
+      DataOut <= RecvReg;               -- read receiver register
+    else
+      DataOut <= StatReg;               -- read status register
+    end if;
+  end process;
+
+  irq <= StatReg(7);
+
+---------------------------------------------------------------
+-- Data Carrier Detect Edge rising edge detect
+---------------------------------------------------------------
+  ACIA_DCD_edge : process(reset, clk)
+  begin
+    if reset = '1' then
+      DCDEdge <= '0';
+      DCDDel  <= '0';
+    elsif falling_edge(clk) then
+      DCDDel  <= DCD_n;
+      DCDEdge <= DCD_n and (not DCDDel);
+    end if;
+  end process;
+
+
+---------------------------------------------------------------
+-- Data Carrier Detect Interrupt
+---------------------------------------------------------------
+-- If Data Carrier is lost, an interrupt is generated
+-- To clear the interrupt, first read the status register
+--      then read the data receive register
+
+  ACIA_DCD_Int : process(reset, clk)
+  begin
+    if reset = '1' then
+      DCDInt   <= '0';
+      DCDState <= DCD_State_Idle;
+    elsif falling_edge(clk) then
+      case DCDState is
+        when DCD_State_Idle =>
+          -- DCD Edge activates interrupt
+          if DCDEdge = '1' then
+            DCDInt   <= '1';
+            DCDState <= DCD_State_Int;
+          end if;
+        when DCD_State_Int =>
+          -- To reset DCD interrupt, 
+          -- First read status
+          if ReadSR = '1' then
+            DCDState <= DCD_State_Reset;
+          end if;
+        when DCD_State_Reset =>
+          -- Then read receive register
+          if ReadRR = '1' then
+            DCDInt   <= '0';
+            DCDState <= DCD_State_Idle;
+          end if;
+        when others =>
+          null;
+      end case;
+    end if;
+  end process;
+
+  rts_n <= RxDR;
+
+end rtl;
+
